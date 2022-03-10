a378b305e177f956456994a192a373683b431dec
黃智輝 <cpmoviephil@gmail.com>
2022-03-10 23:51:37 +0800

    +page indication

--- lib/screens/page/exam.dart
+++ lib/screens/page/exam.dart
@@ -1,20 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class ExamPage extends StatelessWidget {
+import '../../utils/utils.dart';
 
-  const ExamPage({Key? key,}) : super(key: key,);
+class ExamPage extends StatefulWidget {
+
+  const ExamPage({
+    Key? key,
+  }) : super(key: key,);
+
+  @override
+  _ExamPageState createState() => _ExamPageState();
+}
+
+class _ExamPageState extends State<ExamPage> {
+
+  final _pageController = PageController(viewportFraction: 0.8, keepPage: true);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(44, 17, 242, 1),
         title: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text("完成10道題目可解鎖獎勵"),
-
+            WidgetsHelper.appText(
+              text: "完成10道題目可解鎖獎勵",
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 5,),
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5,),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                color: Color.fromRGBO(118, 99, 255, 1),
+              ),
+              child: SmoothPageIndicator(
+                controller: _pageController,
+                count: 10,
+                effect: const ScrollingDotsEffect(
+                  dotColor: Colors.white,
+                  activeDotScale: 1.0,
+                  maxVisibleDots: 11,
+                  radius: 8,
+                  spacing: 6,
+                  dotHeight: 12,
+                  dotWidth: 12,
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+class _ExamPageView extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
